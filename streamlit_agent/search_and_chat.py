ad06e611d223db1a0590951d91cdbf29f609a94d
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-04-18 20:37:15 -0400

    ok

--- streamlit_agent/search_and_chat.py
+++ streamlit_agent/search_and_chat.py
@@ -23,7 +23,7 @@ if len(msgs.messages) == 0 or st.sidebar.button("Effacez votre historique de cha
     msgs.add_ai_message("Dites-moi Comment je peux vous aider?")
     st.session_state.steps = {}
 
-st.sidebar.write("""pour tout vos projets de 
+st.sidebar.write("""🌟pour tout vos projets de 
 Chat avec IA commyuniquez avec:
 [🚀Jose🚀](martianbandit@gmail.com)""")
 
