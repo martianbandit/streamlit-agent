8178126d6891c90d5e1a16ce556056f1692fc52f
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-04-18 21:09:41 -0400

    sidebar

--- streamlit_agent/search_and_chat.py
+++ streamlit_agent/search_and_chat.py
@@ -12,7 +12,7 @@ import streamlit as st
 st.set_page_config(page_title="Gpts-Index Chatbot: Discussions avec recherche web", page_icon="🌟")
 st.title("🤖 Gpts-Index: Chat avec recherche web")
 st.sidebar.title("Chatbot general de 🤖[Gpt-Index](https://gpts-index.com) Venez visiter notre [site web](https://gpts-index.com)")
-st.bar.write("inserrez votre clé API de OpenAI dans la barre d'adresse ci-dessous ⬇️ ")
+st.sidebar.write("inserrez votre clé API de OpenAI dans la barre d'adresse ci-dessous ⬇️ ")
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
 msgs = StreamlitChatMessageHistory()
