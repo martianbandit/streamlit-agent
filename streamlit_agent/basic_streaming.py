c29437672ce4095cfdca9a20f38e40dc7ea1ccf7
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-04-20 22:36:18 -0400

    Update basic_streaming.py

--- streamlit_agent/basic_streaming.py
+++ streamlit_agent/basic_streaming.py
@@ -13,8 +13,8 @@ class StreamHandler(BaseCallbackHandler):
         self.text += token
         self.container.markdown(self.text)
 
-st.title(" :rainbow[Bienvenue sur le site des Chatbots de [Gpts-Index.com](https://gpts-index.com)]")
-st.sidebar.title(" :green[differents modeles, differentes plateformes, des outils et des agents! Selectionnez vos préférence pour chaque attributs.]")
+st.title(":wrench: :rainbow[ Bienvenue sur le site des Chatbots de [Gpts-Index.com](https://gpts-index.com)]")
+st.sidebar.title(" :green[differents modeles, differentes plateformes, des outils et des agents! n/Selectionnez vos préférence pour chaque attributs.]")
 with st.sidebar:
     openai_api_key = st.text_input(" :rainbow[inserrez votre cle API de OpenAI ou sinon [cliquez ici](https://platform.openai.com/api-keys) pour en obtenir une. Il es possible de choisir un autre modèle a partir d'autre plateforme.]", type="password")
     TOGETHER_API_KEY = st.text_input(" :rainbow[Inserrez votre cle API de Together.ai ou obtenez la gratuitement et avoir acces a plus de 100 modeles !! [Obtenir une cle API together.ai ici](https://api.together.xyz/settings/api-keys)]", type="password")
